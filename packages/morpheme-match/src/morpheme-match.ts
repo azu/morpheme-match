21ba626e2c71fff3b93555235f71ea25a2a30342
azu <azuciao@gmail.com>
2019-06-13 21:41:41 +0900

    fix(morpheme-match): Update Token type

--- packages/morpheme-match/src/morpheme-match.ts
+++ packages/morpheme-match/src/morpheme-match.ts
@@ -1,14 +1,29 @@
 export type Token = {
+    // 辞書内での単語ID
+    word_id: number;
+    // 単語タイプ(辞書に登録されている単語ならKNOWN; 未知語ならUNKNOWN)
+    word_type: "KNOWN" | "UNKNOWN";
+    // 表層形
     surface_form: string;
+    // 品詞
     pos: string;
+    // 品詞細分類1
     pos_detail_1: string;
+    // 品詞細分類2
     pos_detail_2: string;
+    // 品詞細分類3
     pos_detail_3: string;
+    // 活用型
     conjugated_type: string;
+    // 活用形
     conjugated_form: string;
+    // 基本形
     basic_form: string;
+    // 読み
     reading: string;
+    // 発音
     pronunciation: string;
+    // 単語の開始位置
     word_position: number;
 };
 
