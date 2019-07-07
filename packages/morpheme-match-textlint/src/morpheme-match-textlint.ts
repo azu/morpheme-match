6e2492721c3cf26cf85de21a67fde0f48a3ba000
azu <azuciao@gmail.com>
2019-07-07 16:56:33 +0900

    fix(textlint): copy type from morpheme-match

--- packages/morpheme-match-textlint/src/morpheme-match-textlint.ts
+++ packages/morpheme-match-textlint/src/morpheme-match-textlint.ts
@@ -1,8 +1,42 @@
 import {createMatcher as createMathAll, ExpectedDictionary, ExpectedDictionaries} from "morpheme-match-all";
 
-type Token = import("morpheme-match").Token;
+export type Token = {
+    // 辞書内での単語ID
+    word_id: number;
+    // 単語タイプ(辞書に登録されている単語ならKNOWN; 未知語ならUNKNOWN)
+    word_type: "KNOWN" | "UNKNOWN";
+    // 表層形
+    surface_form: string;
+    // 品詞
+    pos: string;
+    // 品詞細分類1
+    pos_detail_1: string;
+    // 品詞細分類2
+    pos_detail_2: string;
+    // 品詞細分類3
+    pos_detail_3: string;
+    // 活用型
+    conjugated_type: string;
+    // 活用形
+    conjugated_form: string;
+    // 基本形
+    basic_form: string;
+    // 読み
+    reading: string;
+    // 発音
+    pronunciation: string;
+    // 単語の開始位置
+    word_position: number;
+};
 
-type ExpectedToken = import("morpheme-match").ExpectedToken;
+export type ExpectedTokenAdditional = {
+    _skippable?: boolean;
+};
+
+export type ExpectedToken = Partial<Token> & ExpectedTokenAdditional & {
+    [index: string]: any;
+};
+
 export type ExpectedTokenWithCapture = ExpectedToken & {
     _capture?: string
 }
